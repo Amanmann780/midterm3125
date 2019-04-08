cde31f1e576945e68df74177bc18ab270c3aa633
amanmaan <Amanmann780>
2019-04-08 19:03:17 -0400

    istcommit

--- app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
+++ app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
@@ -16,4 +16,20 @@ public class MainCityActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+    public  void parsonjson()
+    public jsonString = ReadjsonUtils.loadJSONFromAsset(context this, json){
+        String json;
+         }
+JSONObject mJsonWeather = new JSONObject(jsonString);
+    //City Data
+    JSONObject mCity = mJsonWeather.getJSONObject(“city”);
+    //City Count
+    JSONObject mCnt = mJsonWeather.getJSONObject(“cnt”);
+    //City List
+    JSONArray mList = mJsonWeather.getJSONArray(“list”);
+              Log.e(TAG,mCity.toString()+mCnt+mList);
+} catch (JSONException e) {
+        e.printStackTrace();
+        }
+
+//json
