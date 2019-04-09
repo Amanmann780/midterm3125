42199bc1e6dbc048f840dd603fa6b43652ef8179
amanmaan <Amanmann780>
2019-04-08 20:39:34 -0400

    2nd commit

---
+++ app/src/main/java/com/midtermmad3125/city.java
@@ -0,0 +1,27 @@
+package com.midtermmad3125;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class city extends AppCompatActivity {
+    public class City {
+
+        private  int id;
+        private String name;
+        private String lon;
+        private  String lat;
+        private String country;
+        private  String population;
+
+
+
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city);
+    }
+}
--- app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
+++ app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
@@ -1,35 +1,40 @@
 package com.midtermmad3125.ui;
 
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowManager;
 
 import com.midtermmad3125.R;
 import com.midtermmad3125.utils.ReadJSONUtils;
 
 public class MainCityActivity extends AppCompatActivity
 {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public  void parsonjson()
-    public jsonString = ReadjsonUtils.loadJSONFromAsset(context this, json){
-        String json;
-         }
-JSONObject mJsonWeather = new JSONObject(jsonString);
-    //City Data
-    JSONObject mCity = mJsonWeather.getJSONObject(“city”);
-    //City Count
-    JSONObject mCnt = mJsonWeather.getJSONObject(“cnt”);
-    //City List
-    JSONArray mList = mJsonWeather.getJSONArray(“list”);
-              Log.e(TAG,mCity.toString()+mCnt+mList);
-} catch (JSONException e) {
-        e.printStackTrace();
+    public void parseJson() {
+    Log.e(TAG,"Into Parse JSon"
+            String jsonString = ReadJSONUtils.loadJSONFromAsset(this, "moscow_weather.json");
+    if (!jsonString.isEmpty()) {
+        try {
+            JSONObject mJsonWeather = new JSONObject(jsonString);
+            //City Data
+            JSONObject mCity = mJsonWeather.getJSONObject("city");
+            //Data of City
+            int id = mCity.getInt("id");
+            Log.e(TAG, id + "");
+            String name = mCity.getString("name");
+            //Data of City Coord
+            JSONObject mCoord = mCity.getJSONObject("cord");
+            String lon = mCoord.getString("lon");
+            String lat = mCoord.getString("lat");
+            Log.e(TAG, lat + "");
+            //Data of City
+            String country = mCity.getString("country");
+            int population = mCity.getInt("population");
+            //City Count
+            int mCnt = mJsonWeather.getInt("cnt");
+            //City List
         }
-
-//json
+    }}
