9ea9f4be5be4d86108f784e2280419a25a48353c
amanmaan <Amanmann780>
2019-04-08 20:57:43 -0400

    splash screen

---
+++ app/src/main/java/com/midtermmad3125/slashscreen.java
@@ -0,0 +1,22 @@
+package com.midtermmad3125;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SplashWait = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent LoginIntent = new Intent(SplashScreen.this,MainCityActivity.class);
+                startActivity(LoginIntent);
+                finish();
+
+            }
+        },SplashWait);
+    }
+}
--- app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
+++ app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
@@ -11,12 +11,17 @@ import android.view.WindowManager;
 import com.midtermmad3125.R;
 import com.midtermmad3125.utils.ReadJSONUtils;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
 public class MainCityActivity extends AppCompatActivity
 {
     public void parseJson() {
     Log.e(TAG,"Into Parse JSon"
-            String jsonString = ReadJSONUtils.loadJSONFromAsset(this, "moscow_weather.json");
-    if (!jsonString.isEmpty()) {
+            String JsonString = ReadJSONUtils.loadJSONFromAsset(this, "moscow_weather.json");
+    if (!JSONStringer.is Empty() )
+        
         try {
             JSONObject mJsonWeather = new JSONObject(jsonString);
             //City Data
@@ -33,8 +38,40 @@ public class MainCityActivity extends AppCompatActivity
             //Data of City
             String country = mCity.getString("country");
             int population = mCity.getInt("population");
-            //City Count
             int mCnt = mJsonWeather.getInt("cnt");
-            //City List
+            JSONArray mListArray = mJsonWeather.getJSONArray("list");
+            for(int i=0;i<mListArray.length();i++) {
+                JSONObject mJsonObjectList = mListArray.getJSONObject(i);
+                int dt = mJsonObjectList.getInt("dt");
+                Log.e(TAG,dt +"dt");
+                JSONObject mTemp = mJsonObjectList.getJSONObject("temp");
+                String min = mTemp.getString("min");
+                String max = mTemp.getString("max");
+                Log.e(TAG,max +"max");
+                String night = mTemp.getString("night");
+                String eve = mTemp.getString("eve");
+                String morn = mTemp.getString("morn");
+                String pressure = mJsonObjectList.getString("pressure");
+                int humidity = mJsonObjectList.getInt("humidity");
+                JSONArray weatherArray = mJsonObjectList.getJSONArray("weather");
+
+                for(int j=0;j<weatherArray.length();j++) {
+                    JSONObject mJsonWeatherList = weatherArray.getJSONObject(j);
+                    int idWheather= mJsonWeatherList.getInt("id");
+                    Log.e(TAG,idWheather +"idWheather");
+                    String main = mJsonWeatherList.getString("main");
+                    String description = mJsonWeatherList.getString("description");
+                    String icon = mJsonWeatherList.getString("icon");
+                }
+                String speed = mJsonObjectList.getString("speed");
+                Log.e(TAG,speed +"speed");
+                int deg = mJsonObjectList.getInt("deg");
+                int clouds = mJsonObjectList.getInt("clouds");
+                Log.e(TAG,clouds +"clouds");
+
+                //String rain = mJsonObjectList.getString(“rain”);
+                //Log.e(TAG,rain +“clouds”);
+
+
+            }
         }
-    }}
