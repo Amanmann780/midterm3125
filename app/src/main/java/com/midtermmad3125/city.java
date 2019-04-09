8a3afdb4f3b3fd773320b4919d07d31fc4066e02
amanmaan <Amanmann780>
2019-04-08 21:32:25 -0400

    city longitude,lat,pop

--- app/src/main/java/com/midtermmad3125/city.java
+++ app/src/main/java/com/midtermmad3125/city.java
@@ -2,19 +2,22 @@ package com.midtermmad3125;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.StaticLayout;
+
+import java.util.ArrayList;
 
 public class city extends AppCompatActivity {
-    public class City {
+    public static class City {
 
-        private  int id;
+        private int id;
         private String name;
         private String lon;
-        private  String lat;
+        private String lat;
         private String country;
-        private  String population;
+        private String population;
 
 
-        public City(int id, String name, String lon, String lat, String country, int population) {
+        public City(int id, String name, String lon, String lat, String country, String population) {
             this.id = id;
             this.name = name;
             this.lon = lon;
@@ -23,14 +26,63 @@ public class city extends AppCompatActivity {
             this.population = population;
         }
 
+        public int getId() {
+            return id;
+        }
 
+        public void setId(int id) {
+            this.id = id;
+        }
 
+        public String getName() {
+            return name;
+        }
 
-    }
+        public void setName(String name) {
+            this.name = name;
+        }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_city);
-    }
-}
+        public String getLon() {
+            return lon;
+        }
+
+        public void setLon(String lon) {
+            this.lon = lon;
+        }
+
+        public String getLat() {
+            return lat;
+        }
+
+        public void setLat(String lat) {
+            this.lat = lat;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public String getPopulation() {
+            return population;
+        }
+
+        public void setPopulation(String population) {
+            this.population = population;
+        }
+
+        private static ArrayList<City> cityList = new ArrayList<City>();
+
+        public static void createArrayList(City city) {
+            cityList.add(city);
+        }
+
+        public static ArrayList<City> getArrayList() {
+            return cityList;
+        }
+
+    }}
+
