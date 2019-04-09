73937d470b954966d0fed6964ce7594f1d26afc9
amanmaan <Amanmann780>
2019-04-08 21:01:05 -0400

    splash

--- app/src/main/java/com/midtermmad3125/slashscreen.java
+++ app/src/main/java/com/midtermmad3125/slashscreen.java
@@ -1,7 +1,11 @@
 package com.midtermmad3125;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.midtermmad3125.ui.MainCityActivity;
 
 public class SplashScreen extends AppCompatActivity {
     private static int SplashWait = 4000;
@@ -12,7 +16,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent LoginIntent = new Intent(SplashScreen.this,MainCityActivity.class);
+                Intent LoginIntent = new Intent(SplashScreen.this, MainCityActivity.class);
                 startActivity(LoginIntent);
                 finish();
 
